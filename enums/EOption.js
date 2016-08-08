2d50d4d9b6a22f67790fa9ab61587b91c67c7705
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-08-08 01:27:29 -0700

    Add enums

---
+++ enums/EOption.js
@@ -0,0 +1,13 @@
+/**
+ * @enum EOption
+ */
+module.exports = {
+	"ROUND_TIME": 1,
+	"BOMB_TIME": 2,
+	"FREEZE_TIME": 3,
+
+	// Value-to-name mapping for convenience
+	"1": "ROUND_TIME",
+	"2": "BOMB_TIME",
+	"3": "FREEZE_TIME",
+};
---
+++ enums/ERoundType.js
@@ -0,0 +1,19 @@
+/**
+ * @enum ERoundType
+ */
+module.exports = {
+	"Terrorists_Win": 1,
+	"CTs_Win": 2,
+	"Bomb_Defused": 3,
+	"Target_Bombed": 4,
+	"Target_Saved": 5,
+	"lost": 6,
+
+	// Value-to-name mapping for convenience
+	"1": "Terrorists_Win",
+	"2": "CTs_Win",
+	"3": "Bomb_Defused",
+	"4": "Target_Bombed",
+	"5": "Target_Saved",
+	"6": "lost",
+};
